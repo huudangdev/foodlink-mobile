5b13be8e7f528054efb255b7aceef58f0b9028f3
Trong Tran <nguyentrongdp192@gmail.com>
2025-02-16 22:32:24 +0700

    Refactor: Update project configuration and assets for release

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,248 +1,248 @@
-import React, { useEffect, useState } from "react";
-import { Link, router, Tabs, useNavigation } from "expo-router";
+import React, { useEffect, useState } from 'react';
+import { Link, router, Tabs, useNavigation } from 'expo-router';
 import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Appearance,
-} from "react-native";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	Appearance,
+} from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-import Colors from "@/constants/Colors";
-import { useColorScheme } from "@/components/useColorScheme";
-import { useClientOnlyValue } from "@/components/useClientOnlyValue";
-import { Image } from "react-native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import OrderHeader from "@/components/headers/OrderHeader";
-import { BottomTabHeaderProps } from "@react-navigation/bottom-tabs";
-import logo from "@/assets/images/logo.jpg";
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
+import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { Image } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import OrderHeader from '@/components/headers/OrderHeader';
+import { BottomTabHeaderProps } from '@react-navigation/bottom-tabs';
+import logo from '@/assets/images/logo.png';
 
 const Drawer = createDrawerNavigator();
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
-  color: string;
-  focused: boolean;
+	name: React.ComponentProps<typeof FontAwesome>['name'];
+	color: string;
+	focused: boolean;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+	return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 const handleNotificationPress = () => {
-  router.push("/screens/NotificationScreen");
+	router.push('/screens/NotificationScreen');
 };
 
 function HomeHeader() {
-  return (
-    <View style={styles.headerContainer}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: "100%",
-          paddingHorizontal: 20,
-          alignItems: "center",
-        }}
-      >
-        <TouchableOpacity
-          onPress={() => {
-            router.push("/screens/DrawerScreen");
-          }}
-        >
-          <Image
-            source={require("../../public/icons/bar.png")}
-            style={{ width: 40, height: 40 }}
-          />
-        </TouchableOpacity>
-        <Image source={logo} style={{ width: 60, height: 30 }} />
-        <TouchableOpacity
-          style={styles.notificationContainer}
-          onPress={handleNotificationPress}
-        >
-          <Image
-            source={require("../../public/icons/notification.png")}
-            style={{ width: 40, height: 40 }}
-          />
-          {/* <View style={styles.badge}>
+	return (
+		<View style={styles.headerContainer}>
+			<View
+				style={{
+					flexDirection: 'row',
+					justifyContent: 'space-between',
+					width: '100%',
+					paddingHorizontal: 20,
+					alignItems: 'center',
+				}}
+			>
+				<TouchableOpacity
+					onPress={() => {
+						router.push('/screens/DrawerScreen');
+					}}
+				>
+					<Image
+						source={require('../../public/icons/bar.png')}
+						style={{ width: 40, height: 40 }}
+					/>
+				</TouchableOpacity>
+				<Image source={logo} style={{ width: 60, height: 30 }} />
+				<TouchableOpacity
+					style={styles.notificationContainer}
+					onPress={handleNotificationPress}
+				>
+					<Image
+						source={require('../../public/icons/notification.png')}
+						style={{ width: 40, height: 40 }}
+					/>
+					{/* <View style={styles.badge}>
             <Text style={styles.badgeText}>0</Text>
           </View> */}
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  const navigation = useNavigation();
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const navigation = useNavigation();
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  //const { token } = useAuth();
+	//const { token } = useAuth();
 
-  // useEffect(() => {
-  //   if (!token) {
-  //     router.push("/screens/Auth/LoginScreen");
-  //   }
-  // }, [token]);
+	// useEffect(() => {
+	//   if (!token) {
+	//     router.push("/screens/Auth/LoginScreen");
+	//   }
+	// }, [token]);
 
-  // if (!token) {
-  //   return null; // Hoặc bạn có thể hiển thị một màn hình chờ
-  // }
-  useEffect(() => {
-    // Buộc sử dụng chế độ sáng
-    Appearance.setColorScheme("light");
-  }, []);
+	// if (!token) {
+	//   return null; // Hoặc bạn có thể hiển thị một màn hình chờ
+	// }
+	useEffect(() => {
+		// Buộc sử dụng chế độ sáng
+		Appearance.setColorScheme('light');
+	}, []);
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: useClientOnlyValue(false, true),
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Trang chủ",
-          header: HomeHeader,
-          tabBarIcon: ({ color, focused }) => (
-            <View>
-              {focused ? (
-                <Image
-                  source={require("../../public/icons/home-active.png")}
-                  style={{ width: 24, height: 24 }}
-                />
-              ) : (
-                <Image
-                  source={require("../../public/icons/home.png")}
-                  style={{ width: 24, height: 24 }}
-                />
-              )}
-              {/* <View style={styles.badge}>
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+				headerShown: useClientOnlyValue(false, true),
+			}}
+		>
+			<Tabs.Screen
+				name='index'
+				options={{
+					title: 'Trang chủ',
+					header: HomeHeader,
+					tabBarIcon: ({ color, focused }) => (
+						<View>
+							{focused ? (
+								<Image
+									source={require('../../public/icons/home-active.png')}
+									style={{ width: 24, height: 24 }}
+								/>
+							) : (
+								<Image
+									source={require('../../public/icons/home.png')}
+									style={{ width: 24, height: 24 }}
+								/>
+							)}
+							{/* <View style={styles.badge}>
                 <Text style={styles.badgeText}>0</Text>
               </View> */}
-            </View>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="orders"
-        options={{
-          title: "Đơn hàng",
-          header: (props: BottomTabHeaderProps) => (
-            <OrderHeader
-              open={false}
-              setOpen={function (value: React.SetStateAction<boolean>): void {
-                throw new Error("Function not implemented.");
-              }}
-              selectedPlatform={""}
-              handlePlatformChange={function (value: string): void {
-                throw new Error("Function not implemented.");
-              }}
-              {...props}
-            />
-          ),
-          tabBarIcon: ({ color, focused }) =>
-            focused ? (
-              <Image
-                source={require("../../public/icons/order-active.png")}
-                style={{ width: 24, height: 24 }}
-              />
-            ) : (
-              <Image
-                source={require("../../public/icons/order.png")}
-                style={{ width: 24, height: 24 }}
-              />
-            ),
-        }}
-      />
-      <Tabs.Screen
-        name="reports"
-        options={{
-          title: "Báo cáo",
-          tabBarIcon: ({ color, focused }) =>
-            focused ? (
-              <Image
-                source={require("../../public/icons/report-active.png")}
-                style={{ width: 24, height: 24 }}
-              />
-            ) : (
-              <Image
-                source={require("../../public/icons/report.png")}
-                style={{ width: 24, height: 24 }}
-              />
-            ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Tài khoản",
-          tabBarIcon: ({ color, focused }) =>
-            focused ? (
-              <Image
-                source={require("../../public/icons/user-active.png")}
-                style={{ width: 24, height: 24 }}
-              />
-            ) : (
-              <Image
-                source={require("../../public/icons/user.png")}
-                style={{ width: 24, height: 24 }}
-              />
-            ),
-        }}
-      />
-    </Tabs>
-  );
+						</View>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name='orders'
+				options={{
+					title: 'Đơn hàng',
+					header: (props: BottomTabHeaderProps) => (
+						<OrderHeader
+							open={false}
+							setOpen={function (value: React.SetStateAction<boolean>): void {
+								throw new Error('Function not implemented.');
+							}}
+							selectedPlatform={''}
+							handlePlatformChange={function (value: string): void {
+								throw new Error('Function not implemented.');
+							}}
+							{...props}
+						/>
+					),
+					tabBarIcon: ({ color, focused }) =>
+						focused ? (
+							<Image
+								source={require('../../public/icons/order-active.png')}
+								style={{ width: 24, height: 24 }}
+							/>
+						) : (
+							<Image
+								source={require('../../public/icons/order.png')}
+								style={{ width: 24, height: 24 }}
+							/>
+						),
+				}}
+			/>
+			<Tabs.Screen
+				name='reports'
+				options={{
+					title: 'Báo cáo',
+					tabBarIcon: ({ color, focused }) =>
+						focused ? (
+							<Image
+								source={require('../../public/icons/report-active.png')}
+								style={{ width: 24, height: 24 }}
+							/>
+						) : (
+							<Image
+								source={require('../../public/icons/report.png')}
+								style={{ width: 24, height: 24 }}
+							/>
+						),
+				}}
+			/>
+			<Tabs.Screen
+				name='profile'
+				options={{
+					title: 'Tài khoản',
+					tabBarIcon: ({ color, focused }) =>
+						focused ? (
+							<Image
+								source={require('../../public/icons/user-active.png')}
+								style={{ width: 24, height: 24 }}
+							/>
+						) : (
+							<Image
+								source={require('../../public/icons/user.png')}
+								style={{ width: 24, height: 24 }}
+							/>
+						),
+				}}
+			/>
+		</Tabs>
+	);
 }
 
 const styles = StyleSheet.create({
-  badge: {
-    position: "absolute",
-    right: -6,
-    top: -3,
-    backgroundColor: "#FEAA00",
-    borderRadius: 6,
-    width: 14,
-    height: 14,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  notificationContainer: {
-    position: "relative",
-  },
-  badgeText: {
-    color: "white",
-    fontSize: 8,
-    fontWeight: "bold",
-  },
-  headerContainer: {
-    justifyContent: "center",
-    backgroundColor: "white",
-    alignItems: "center",
-    paddingTop: 40,
-    height: 60,
-  },
-  headerTitle: {
-    color: "dark",
-    fontSize: 16,
-  },
-  drawerContent: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "#fff",
-  },
-  header: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  menuItem: {
-    marginBottom: 15,
-  },
-  subMenu: {
-    marginLeft: 10,
-  },
+	badge: {
+		position: 'absolute',
+		right: -6,
+		top: -3,
+		backgroundColor: '#FEAA00',
+		borderRadius: 6,
+		width: 14,
+		height: 14,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	notificationContainer: {
+		position: 'relative',
+	},
+	badgeText: {
+		color: 'white',
+		fontSize: 8,
+		fontWeight: 'bold',
+	},
+	headerContainer: {
+		justifyContent: 'center',
+		backgroundColor: 'white',
+		alignItems: 'center',
+		paddingTop: 40,
+		height: 60,
+	},
+	headerTitle: {
+		color: 'dark',
+		fontSize: 16,
+	},
+	drawerContent: {
+		flex: 1,
+		padding: 20,
+		backgroundColor: '#fff',
+	},
+	header: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		marginBottom: 20,
+	},
+	menuItem: {
+		marginBottom: 15,
+	},
+	subMenu: {
+		marginLeft: 10,
+	},
 });
--- app/screens/Auth/LoginScreen.tsx
+++ app/screens/Auth/LoginScreen.tsx
@@ -1,167 +1,167 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-  Image,
-  Alert,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import logo from "@/assets/images/logo.jpg";
-import { useEffect } from "react";
-import { login } from "../../services/auth/authApi";
-import { useAuth } from "@/app/context/AuthContext";
+	View,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	StyleSheet,
+	Image,
+	Alert,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import logo from '@/assets/images/logo.png';
+import { useEffect } from 'react';
+import { login } from '../../services/auth/authApi';
+import { useAuth } from '@/app/context/AuthContext';
 
 const LoginScreen = () => {
-  const [phoneNumber, setPhoneNumber] = useState(
-    "dang.nguyentranhuu@gmail.com"
-  );
-  const [password, setPassword] = useState("Uit1657421717;");
-  const [showPassword, setShowPassword] = useState(false);
-  const navigation = useNavigation<any>();
-  const { setToken, setUser } = useAuth();
+	const [phoneNumber, setPhoneNumber] = useState(
+		'dang.nguyentranhuu@gmail.com'
+	);
+	const [password, setPassword] = useState('Uit1657421717;');
+	const [showPassword, setShowPassword] = useState(false);
+	const navigation = useNavigation<any>();
+	const { setToken, setUser } = useAuth();
 
-  const handleLogin = async () => {
-    console.log(
-      "Logging in with phone:",
-      phoneNumber,
-      "and password:",
-      password
-    );
-    try {
-      const response = await login(phoneNumber, password);
-      console.log("Login successful:", response.token, response.user.username);
+	const handleLogin = async () => {
+		console.log(
+			'Logging in with phone:',
+			phoneNumber,
+			'and password:',
+			password
+		);
+		try {
+			const response = await login(phoneNumber, password);
+			console.log('Login successful:', response.token, response.user.username);
 
-      setToken(response.token); // Lưu token vào bộ nhớ cục bộ
-      setUser(response.user); // Lưu thông tin người dùng vào bộ nhớ cục bộ
+			setToken(response.token); // Lưu token vào bộ nhớ cục bộ
+			setUser(response.user); // Lưu thông tin người dùng vào bộ nhớ cục bộ
 
-      // Điều hướng đến màn hình chính hoặc màn hình khác sau khi đăng nhập thành công
-      navigation.navigate("(tabs)");
-    } catch (error) {
-      Alert.alert("Error", "Đăng nhập thất bại, sai tài khoản hoặc mật khẩu.");
-    }
-  };
+			// Điều hướng đến màn hình chính hoặc màn hình khác sau khi đăng nhập thành công
+			navigation.navigate('(tabs)');
+		} catch (error) {
+			Alert.alert('Error', 'Đăng nhập thất bại, sai tài khoản hoặc mật khẩu.');
+		}
+	};
 
-  return (
-    <View style={styles.container}>
-      <Image source={logo} style={styles.logo} />
+	return (
+		<View style={styles.container}>
+			<Image source={logo} style={styles.logo} />
 
-      {/* Title */}
-      <Text style={styles.title}>Đăng nhập</Text>
-      <Text style={styles.subtitle}>
-        Vui lòng nhập số điện thoại/email và mật khẩu để đăng nhập
-      </Text>
+			{/* Title */}
+			<Text style={styles.title}>Đăng nhập</Text>
+			<Text style={styles.subtitle}>
+				Vui lòng nhập số điện thoại/email và mật khẩu để đăng nhập
+			</Text>
 
-      {/* Input Fields */}
-      <Text style={styles.label}>Số điện thoại / Email</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nhập số điện thoại"
-        value={phoneNumber}
-        onChangeText={setPhoneNumber}
-        keyboardType="phone-pad"
-      />
+			{/* Input Fields */}
+			<Text style={styles.label}>Số điện thoại / Email</Text>
+			<TextInput
+				style={styles.input}
+				placeholder='Nhập số điện thoại'
+				value={phoneNumber}
+				onChangeText={setPhoneNumber}
+				keyboardType='phone-pad'
+			/>
 
-      <Text style={styles.label}>Mật khẩu</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nhập mật khẩu"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry={!showPassword}
-      />
+			<Text style={styles.label}>Mật khẩu</Text>
+			<TextInput
+				style={styles.input}
+				placeholder='Nhập mật khẩu'
+				value={password}
+				onChangeText={setPassword}
+				secureTextEntry={!showPassword}
+			/>
 
-      {/* Quên mật khẩu */}
-      <TouchableOpacity>
-        <Text style={styles.forgotPassword}>Quên mật khẩu?</Text>
-      </TouchableOpacity>
+			{/* Quên mật khẩu */}
+			<TouchableOpacity>
+				<Text style={styles.forgotPassword}>Quên mật khẩu?</Text>
+			</TouchableOpacity>
 
-      {/* Đăng nhập button */}
-      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-        <Text style={styles.loginButtonText}>Đăng nhập</Text>
-      </TouchableOpacity>
+			{/* Đăng nhập button */}
+			<TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+				<Text style={styles.loginButtonText}>Đăng nhập</Text>
+			</TouchableOpacity>
 
-      {/* Footer */}
-      <View style={styles.footer}>
-        <Text>Bạn chưa có tài khoản? </Text>
-        <TouchableOpacity>
-          <Text style={styles.contactUs}>Hãy liên hệ chúng tôi</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+			{/* Footer */}
+			<View style={styles.footer}>
+				<Text>Bạn chưa có tài khoản? </Text>
+				<TouchableOpacity>
+					<Text style={styles.contactUs}>Hãy liên hệ chúng tôi</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    paddingTop: 140,
-    backgroundColor: "#fff",
-  },
-  logo: {
-    width: 120,
-    height: 60,
-    resizeMode: "contain",
-    alignSelf: "flex-start",
-    marginBottom: 20,
-    marginLeft: -16,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "left",
-    marginBottom: 8,
-    marginTop: 8,
-  },
-  subtitle: {
-    fontSize: 14,
-    color: "#666",
-    textAlign: "left",
-    marginBottom: 20,
-  },
-  label: {
-    fontSize: 14,
-    marginBottom: 8,
-    color: "#333",
-  },
-  input: {
-    height: 48,
-    borderColor: "#D3D3D3",
-    borderWidth: 0.5,
-    borderRadius: 10,
-    paddingHorizontal: 12,
-    marginBottom: 16,
-  },
-  inputFocused: {
-    borderColor: "#FFA500",
-  },
-  forgotPassword: {
-    alignSelf: "flex-end",
-    color: "#FFA500",
-    marginBottom: 20,
-  },
-  loginButton: {
-    backgroundColor: "#FFA500",
-    padding: 12,
-    borderRadius: 5,
-    alignItems: "center",
-  },
-  loginButtonText: {
-    color: "#fff",
-    fontSize: 16,
-  },
-  footer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: 20,
-  },
-  contactUs: {
-    color: "#FFA500",
-    fontWeight: "bold",
-  },
+	container: {
+		flex: 1,
+		padding: 24,
+		paddingTop: 140,
+		backgroundColor: '#fff',
+	},
+	logo: {
+		width: 120,
+		height: 60,
+		resizeMode: 'contain',
+		alignSelf: 'flex-start',
+		marginBottom: 20,
+		marginLeft: -16,
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		textAlign: 'left',
+		marginBottom: 8,
+		marginTop: 8,
+	},
+	subtitle: {
+		fontSize: 14,
+		color: '#666',
+		textAlign: 'left',
+		marginBottom: 20,
+	},
+	label: {
+		fontSize: 14,
+		marginBottom: 8,
+		color: '#333',
+	},
+	input: {
+		height: 48,
+		borderColor: '#D3D3D3',
+		borderWidth: 0.5,
+		borderRadius: 10,
+		paddingHorizontal: 12,
+		marginBottom: 16,
+	},
+	inputFocused: {
+		borderColor: '#FFA500',
+	},
+	forgotPassword: {
+		alignSelf: 'flex-end',
+		color: '#FFA500',
+		marginBottom: 20,
+	},
+	loginButton: {
+		backgroundColor: '#FFA500',
+		padding: 12,
+		borderRadius: 5,
+		alignItems: 'center',
+	},
+	loginButtonText: {
+		color: '#fff',
+		fontSize: 16,
+	},
+	footer: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		marginTop: 20,
+	},
+	contactUs: {
+		color: '#FFA500',
+		fontWeight: 'bold',
+	},
 });
 
 export default LoginScreen;
